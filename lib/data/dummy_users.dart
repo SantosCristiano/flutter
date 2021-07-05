975795fae1d42edcdc29be2301f52eaa8fd7df4f
Cristiano Santos <santosdesouzacris@gmail.com>
2021-07-05 19:45:02 -0300

    Up changes Flutter

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Cris & Cristiano',
+    email: 'cris@teckins.com',
+    avatarUrl: 'https://scontent.fcgh19-1.fna.fbcdn.net/v/t1.6435-9/176880713_2181169522019445_2592569970884370072_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=a_Vp4luL7QEAX_XWpsj&_nc_ht=scontent.fcgh19-1.fna&oh=76751c690e674efbf0583eca415d2dc3&oe=60E93C41'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@zymail.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png'
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guigui@abcmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+  )
+};
